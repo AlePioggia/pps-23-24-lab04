d61f4e6c2fa31ab34d78df91d21c0d3e4c27d7c7
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-04-02 09:19:51 +0200

    fix style

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -2,7 +2,7 @@ package tasks.adts
 
 package u04lab
 
-/*  Exercise 1: 
+/*  Exercise 1:
  *  Complete the implementation of ComplexADT trait below, so that it passes
  *  the test in ComplexTest.
  */
@@ -25,19 +25,18 @@ object Ex1ComplexNumbers:
     case class Complex(re: Double, im: Double)
     def complex(re: Double, im: Double): Complex = Complex(re, im)
     extension (complex: Complex)
-      def re(): Double = complex.re  
+      def re(): Double = complex.re
       def im(): Double = complex.im
       def sum(other: Complex): Complex = other match
         case Complex(re, im) => Complex(complex.re + re, complex.im + im)
 
       def subtract(other: Complex): Complex = other match
         case Complex(re, im) => Complex(complex.re - re, complex.im - im)
-      
-      def asString(): String = complex match
-        case Complex(re, im) if re != 0 && im > 0 
-        => re.toString().concat(" + ".concat(im.toString().concat("i")))
-        case Complex(re, im) if re != 0 && im < 0 
-        => complex.re.toString().concat(" - ".concat(Math.abs(im).toString().concat("i")))
-        
 
-      
+      def asString(): String = complex match
+        case Complex(re, im) if im > 0 =>
+          re.toString().concat(" + ".concat(im.toString().concat("i")))
+        case Complex(re, im) if im < 0 =>
+          complex.re
+            .toString()
+            .concat(" - ".concat(Math.abs(im).toString().concat("i")))
